7e9af78114acb551d08240a0935c7128df70cdf8
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-06 02:04:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def long_planeteer_calls(array)
   array.any?{|word|word.length > 4}
 end
 
+
 def find_the_cheese(some_array)
   some_array.any?{|x| ["cheddar", "gouda", "camembert"].include?(x)}
 end
