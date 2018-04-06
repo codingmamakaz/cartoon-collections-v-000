89906b45cb05251499f975b4e94e4459c2640129
Kazumi Karbowski <kazkazinus@gmail.com>
2018-04-06 02:04:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ def long_planeteer_calls(array)
   array.any?{|word|word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(some_array)
+  some_array.any?{|x| ["cheddar", "gouda", "camembert"].include?(x)}
 end
